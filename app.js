d8b017f8cb6fc45ba218ab6ec4edbf6dcaf06eaa
Devendra Singh <devendrasamant8505@gmail.com>
2024-08-03 03:43:34 +0530

    patch: integrating bundler, playing with Parcel as a bundler

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-// const heading = React.createElement("h1",{ id: "title"}, "Heading 1");
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const heading2 = React.createElement("h2", { id: "title"}, "Heading 1")
+const heading2 = React.createElement(
+    "h2", 
+    { 
+        id: "title"
+    }, 
+    "Heading 1"
+)
 
-const heading = React.createElement("h1",{ id: "title1"}, "Heading 2");
+const heading = React.createElement(
+    "h1",
+    { 
+        id: "title1"
+    }, 
+    "Heading 2"
+);
 
 const container = React.createElement(
     "div", 
@@ -15,8 +28,4 @@ const container = React.createElement(
 console.log(container);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-//passing a react component inside root element 
-
-//async defer
 root.render(container);
