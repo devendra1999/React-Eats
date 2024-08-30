5dab458c532499236ffac80ac8adf0b072998b8a
Devendra Singh <devendrasamant8505@gmail.com>
2024-08-30 23:05:26 +0530

    add: class play

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
+
 const Profile = (props) => {
+
   const [count, setCount] = useState(0);
+  
   useEffect(() => {
     // API Call
     const timer = setInterval(() => {
@@ -12,10 +15,12 @@ const Profile = (props) => {
       console.log("useEffect Return");
     };
   }, []);
+
   console.log("render");
+
   return (
     <div>
-      <h2>Porofile Component</h2>
+      <h2>Profile Component</h2>
       <h3>Name: {props.name}</h3>
       <h3>Count: {count}</h3>
       <button
